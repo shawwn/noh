7a0e28682e52ac8d92bbb7621f46a3d226389c76
Shawn Presser <shawnpresser@gmail.com>
2023-05-04 23:40:44 -0500

    Ignore deprecation warning (CGDisplayIOServicePort)

--- src/k2/c_system_osx.mm
+++ src/k2/c_system_osx.mm
@@ -1786,7 +1786,10 @@ SSysInfo    CSystem::GetSystemInfo()
         }
         
         // Video Card
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
         io_registry_entry_t displayport = CGDisplayIOServicePort(kCGDirectMainDisplay);
+#pragma clang diagnostic pop
         CFDataRef model = (CFDataRef)IORegistryEntrySearchCFProperty(displayport, kIOServicePlane, CFSTR("model"), kCFAllocatorDefault, kIORegistryIterateRecursively | kIORegistryIterateParents);
         if (model)
         {
