8ca110483884743bd4489a02ac92e486288bbb29
Shawn Presser <shawnpresser@gmail.com>
2023-05-04 23:41:07 -0500

    Ignore deprecation warning (ProcessInformationCopyDictionary)

--- src/k2/c_system_osx.mm
+++ src/k2/c_system_osx.mm
@@ -2024,7 +2024,10 @@ tstring CSystem::GetProcessFilename()
 {
     ProcessSerialNumber psn = { 0, kCurrentProcess };
     ProcessInfoRec pir;
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
     CFDictionaryRef pProcessInfo(ProcessInformationCopyDictionary(&psn, kProcessDictionaryIncludeAllInformationMask));
+#pragma clang diagnostic pop
     tstring sReturn(TSNULL);
     if (CFDictionaryContainsKey(pProcessInfo, kCFBundleExecutableKey))
     {
